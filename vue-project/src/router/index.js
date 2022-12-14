1d3968f7d3e8d0026e87985cab13ecdfc36f6218
lef237 <lef237k5@gmail.com>
2022-12-14 15:23:27 +0900

    個別のメモごとの編集画面を作った

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 // import HomeView from "../views/HomeView.vue";
 import MemoList from "../views/MemoList.vue";
 import NewView from "../views/NewView.vue";
+import EditView from "../views/EditView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/new",
       name: "new",
       component: NewView,
+    },
+    {
+      path: "/edit/:id",
+      name: "edit",
+      component: EditView,
     },
     // {
     //   path: "/about",
---
+++ vue-project/src/views/EditView.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      memos: [],
+    };
+  },
+  // ここをmountedにすると、下のmemo.titleが読み込まれずエラーが出るので注意
+  created() {
+    this.memos = JSON.parse(localStorage.getItem("memos") || "[]");
+  },
+  computed: {
+    memo() {
+      let id = parseInt(this.$route.params.id);
+      // console.log(id);
+      // console.log(this.memos);
+      // console.log(this.memos[id]);
+      return this.memos[id];
+    },
+  },
+};
+</script>
+
+<template>
+  <!-- Edit View: {{ $route.params.id }} -->
+  <!-- <p>{{ memo }}</p> -->
+  <p>{{ memo.title }}</p>
+</template>
